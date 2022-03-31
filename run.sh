225066c560c5ad3a34ec5378d998c988fb63f80c
Acatsama0871 <hli113@stevens.edu>
2022-03-31 01:24:10 -0400

    Ready to Test

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
+pip install six
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 gdown 1id9VqzpTT_teP3GpwdFOoMwpc3TRQtiA
 unzip aug_data.zip 
