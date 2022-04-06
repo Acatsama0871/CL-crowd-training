53a963b102579f50dbc51c7975efdf53feb5e0da
HaoHang Li <hli113@stevens.edu>
2022-04-05 23:21:13 -0400

    Update fit_irt.sh

--- fit_irt.sh
+++ fit_irt.sh
@@ -1,14 +1,14 @@
 echo "----------------------------------------------------------------IRT Fit Start----------------------------------------------------------------"
 # Country
-py-irt train 1pl --lr 0.05 --epochs 5000 --priors hierarchical --device cuda /home/haohang/research/CL_upload/total_data/model_judgement/Country.jsonlines /home/haohang/research/CL_upload/total_data/fitted_IRT/Country > /home/haohang/research/CL_upload/total_data/fitted_IRT/Country/Country.txt
+py-irt train 1pl --lr 0.1 --epochs 5000 --priors hierarchical --device cuda total_data/model_judgement/Country.jsonlines total_data/fitted_IRT/Country > total_data/fitted_IRT/Country/Country.txt
 # Perspective
-py-irt train 1pl --lr 0.05 --epochs 5000 --priors hierarchical --device cuda /home/haohang/research/CL_upload/total_data/model_judgement/Perspective.jsonlines /home/haohang/research/CL_upload/total_data/fitted_IRT/Perspective > /home/haohang/research/CL_upload/total_data/fitted_IRT/Perspective/Perspective.txt
+py-irt train 1pl --lr 0.1 --epochs 5000 --priors hierarchical --device cuda total_data/model_judgement/Perspective.jsonlines total_data/fitted_IRT/Perspective > total_data/fitted_IRT/Perspective/Perspective.txt
 # Population
-py-irt train 1pl --lr 0.05 --epochs 5000 --priors hierarchical --device cuda /home/haohang/research/CL_upload/total_data/model_judgement/Population.jsonlines /home/haohang/research/CL_upload/total_data/fitted_IRT/Population > /home/haohang/research/CL_upload/total_data/fitted_IRT/Population/Population.txt
+py-irt train 1pl --lr 0.1 --epochs 5000 --priors hierarchical --device cuda total_data/model_judgement/Population.jsonlines total_data/fitted_IRT/Population > total_data/fitted_IRT/Population/Population.txt
 # Intervention
-py-irt train 1pl --lr 0.05 --epochs 5000 --priors hierarchical --device cuda /home/haohang/research/CL_upload/total_data/model_judgement/Intervention.jsonlines /home/haohang/research/CL_upload/total_data/fitted_IRT/Intervention > /home/haohang/research/CL_upload/total_data/fitted_IRT/Intervention/Intervention.txt
+py-irt train 1pl --lr 0.1 --epochs 5000 --priors hierarchical --device cuda total_data/model_judgement/Intervention.jsonlines total_data/fitted_IRT/Intervention > total_data/fitted_IRT/Intervention/Intervention.txt
 # Study Period
-py-irt train 1pl --lr 0.05 --epochs 5000 --priors hierarchical --device cuda /home/haohang/research/CL_upload/total_data/model_judgement/'Study Period.jsonlines' /home/haohang/research/CL_upload/total_data/fitted_IRT/'Study Period' > /home/haohang/research/CL_upload/total_data/fitted_IRT/'Study Period'/'Study Period.txt'
+py-irt train 1pl --lr 0.1 --epochs 5000 --priors hierarchical --device cuda total_data/model_judgement/'Study Period.jsonlines' total_data/fitted_IRT/'Study Period' > total_data/fitted_IRT/'Study Period'/'Study Period.txt'
 # Sample Size
-py-irt train 1pl --lr 0.05 --epochs 5000 --priors hierarchical --device cuda /home/haohang/research/CL_upload/total_data/model_judgement/'Sample Size.jsonlines' /home/haohang/research/CL_upload/total_data/fitted_IRT/'Sample Size' > /home/haohang/research/CL_upload/total_data/fitted_IRT/'Sample Size'/'Sample Size.txt'
+py-irt train 1pl --lr 0.1 --epochs 5000 --priors hierarchical --device cuda total_data/model_judgement/'Sample Size.jsonlines' total_data/fitted_IRT/'Sample Size' > total_data/fitted_IRT/'Sample Size'/'Sample Size.txt'
 echo "----------------------------------------------------------------IRT Fit End----------------------------------------------------------------"
